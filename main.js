84ead9fbfe35e585519a761ca364694c9310bf91
Jones Ferreira <jones_kelvy@hotmail.com>
2026-06-18 00:53:30 -0300

    Update game scores for Ghana vs Panama and Uzbekistan vs Colombia

--- main.js
+++ main.js
@@ -62,8 +62,8 @@ const matchesData = [
       { id: "austria-jordan", home: "austria", hour: "01:00", away: "jordan", score1: "3", score2: "1", broadcasters: ["CazéTV", "Globo", "SporTV"] },
       { id: "portugal-congo", home: "portugal", hour: "14:00", away: "congo", score1: "1", score2: "1", broadcasters: ["CazéTV"] },
       { id: "england-croatia", home: "england", hour: "17:00", away: "croatia", score1: "4", score2: "2", broadcasters: ["CazéTV", "Globo", "SporTV", "SBT"] },
-      { id: "ghana-panama", home: "ghana", hour: "20:00", away: "panama", score1: "-", score2: "-", broadcasters: ["CazéTV"] },
-      { id: "uzbekistan-colombia", home: "uzbekistan", hour: "23:00", away: "colombia", score1: "-", score2: "-", broadcasters: ["CazéTV", "Globo", "SporTV"] }
+      { id: "ghana-panama", home: "ghana", hour: "20:00", away: "panama", score1: "1", score2: "0", broadcasters: ["CazéTV"] },
+      { id: "uzbekistan-colombia", home: "uzbekistan", hour: "23:00", away: "colombia", score1: "1", score2: "2", broadcasters: ["CazéTV", "Globo", "SporTV"] }
     ]
   },
   {

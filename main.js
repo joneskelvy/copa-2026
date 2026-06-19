e71b7a09468b24317fad72183212b36105141832
Jones Ferreira <jones_kelvy@hotmail.com>
2026-06-18 21:03:05 -0300

    Fix score for Canada vs Qatar match
    Updated the score for the Canada vs Qatar game.

--- main.js
+++ main.js
@@ -72,7 +72,7 @@ const matchesData = [
     games: [
       { id: "czech-southafrica", home: "czech", hour: "13:00", away: "southafrica", score1: "1", score2: "1", broadcasters: ["CazéTV"] },
       { id: "switzerland-bosnia", home: "switzerland", hour: "16:00", away: "bosnia", score1: "4", score2: "1", broadcasters: ["CazéTV", "Globo", "SporTV", "SBT"] },
-      { id: "canada-qatar", home: "canada", hour: "19:00", away: "qatar", score1: "-", score2: "-", broadcasters: ["CazéTV"] },
+      { id: "canada-qatar", home: "canada", hour: "19:00", away: "qatar", score1: "6", score2: "0", broadcasters: ["CazéTV"] },
       { id: "mexico-southkorea", home: "mexico", hour: "22:00", away: "southkorea", score1: "-", score2: "-", broadcasters: ["CazéTV", "Globo", "SporTV"] }
     ]
   },

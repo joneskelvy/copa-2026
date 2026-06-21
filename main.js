fe835707ff7775d1880f2f2eb07a807b6b62f55d
Jones Ferreira <jones_kelvy@hotmail.com>
2026-06-21 18:01:27 -0300

    Update main.js

--- main.js
+++ main.js
@@ -101,7 +101,7 @@ const matchesData = [
     games: [
       { id: "tunisia-japan", home: "tunisia", hour: "01:00", away: "japan", score1: "0", score2: "4", broadcasters: ["CazéTV", "Globo", "SporTV"] },
       { id: "spain-saudia", home: "spain", hour: "13:00", away: "saudia", score1: "4", score2: "0", broadcasters: ["CazéTV"] },
-      { id: "belgium-iran", home: "belgium", hour: "16:00", away: "iran", score1: "-", score2: "-", broadcasters: ["CazéTV"] },
+      { id: "belgium-iran", home: "belgium", hour: "16:00", away: "iran", score1: "0", score2: "0", broadcasters: ["CazéTV"] },
       { id: "uruguay-capeverde", home: "uruguay", hour: "19:00", away: "capeverde", score1: "-", score2: "-", broadcasters: ["CazéTV", "Globo", "SporTV", "SBT"] },
       { id: "newzealand-egypt", home: "newzealand", hour: "22:00", away: "egypt", score1: "-", score2: "-", broadcasters: ["CazéTV", "Globo", "SporTV"] }
     ]
